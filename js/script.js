3e8d554819a8d8ddfd25815b6e7b843b367df845
martinagiammillari <martigiammi@gmail.com>
2025-10-29 09:43:52 +0100

    feat: modifica con 1 variabile let

--- js/script.js
+++ js/script.js
@@ -29,20 +29,22 @@
 
 for (let i = 1; i <= 100; i++) {
 
+    let result = ""
 
     if ((i % 3 === 0) && (i % 5 === 0)) {
-        console.log("FizzBuzz")
+        result = "FizzBuzz"
     }
 
     else if (i % 3 === 0) {
-        console.log("Fizz")
+        result = "Fizz"
     }
 
     else if (i % 5 === 0) {
-        console.log("Buzz")
+        result = "Buzz"
     }
     else {
-        console.log(i)
+        result = i
     }
 
+    console.log(result)
 }
